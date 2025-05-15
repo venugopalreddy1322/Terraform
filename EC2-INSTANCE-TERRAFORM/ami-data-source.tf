132163677a832bbc139a1510f05de02175a23845
Venu_DevOps <venugopalreddy1322@gmail.com>
2025-05-15 22:35:56 +0200

    more files updated and few files added

---
+++ EC2-INSTANCE-TERRAFORM/ami-data-source.tf
@@ -0,0 +1,56 @@
+data "aws_ami" "amz2ami" {
+  most_recent = true
+  owners      = ["137112412989"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-kernel-5.10-hvm-2.0.2025*.0-x86_64-gp2"] #Free-tier eligible
+  }
+  filter {
+    name   = "is-public"
+    values = ["true"]
+  }
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+}
+
+/*data "aws_ami" "ubuntu_ami" {
+  most_recent = true
+  owners      = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-*-amd64-server-*", "ubuntu/images/hvm-ssd-gp3/ubuntu-*-amd64-server-*"]
+    #values = ["ubuntu/images/hvm-ssd/ubuntu-*-amd64-server-*"]
+  }
+  
+  filter {
+    name   = "is-public"
+    values = ["true"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+}
+*/
