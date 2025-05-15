fa945cced0483f953a3b872a61bea1aa76bb0e55
Venu_DevOps <venugopalreddy1322@gmail.com>
2025-05-15 22:40:12 +0200

    more files updated and few files added

---
+++ EC2-INSTANCE-TERRAFORM/ec2-securitygroups.tf
@@ -0,0 +1,42 @@
+resource "aws_security_group" "allow_http_ssh" {
+  name        = "allow_http"
+  description = "Allow HTTP on port-80 and SSH on port 22 inbound traffic and all outbound traffic"
+  #vpc_id      = aws_vpc.main.id
+  tags = {
+    Name = "allow_http_ssh"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_http_ipv4" {
+  security_group_id = aws_security_group.allow_http_ssh.id
+  #cidr_ipv4         = aws_vpc.main.cidr_block
+  cidr_ipv4 = "0.0.0.0/0"
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh_ipv4" {
+  security_group_id = aws_security_group.allow_http_ssh.id
+  #cidr_ipv4         = aws_vpc.main.cidr_block
+  cidr_ipv4 = "0.0.0.0/0"
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_jenkins" {
+  security_group_id = aws_security_group.allow_jenkins.id
+  cidr_ipv4 = "0.0.0.0/0"
+  from_port = 8080
+  ip_protocol = "tcp"
+  to_port = 8080
+}
+# Egress Rule
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.allow_http_ssh.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
+
+
