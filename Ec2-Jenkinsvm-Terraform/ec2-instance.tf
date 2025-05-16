7d3791be644e9147f98a33f930506e840ede7802
Venu_DevOps <venugopalreddy1322@gmail.com>
2025-05-16 11:38:27 +0200

    more files updated and few files added

---
+++ Ec2-Jenkinsvm-Terraform/ec2-instance.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "Webserver" {
+  ami           = data.aws_ami.amz2ami.id
+  instance_type = var.instance_type
+  key_name = var.keypair
+  user_data     = file("userdata.sh")
+  vpc_security_group_ids = [aws_security_group.allow_http_ssh.id]
+  tags = {
+    "name" = "Amazon-Linux-Webserver"
+  }
+}
