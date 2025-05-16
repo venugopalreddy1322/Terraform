ba9d57af6422a10e7e6a88184cbc1fa8f2a73411
Venu_DevOps <venugopalreddy1322@gmail.com>
2025-05-16 12:16:06 +0200

    more files updated and few files added

---
+++ Ec2-Jenkinsvm-Terraform/README.md
@@ -0,0 +1,5 @@
+# Creating a EC2 Instance using Terraform
+Instance details:
+1. Ami with free tier eligible
+2. Variableized terraform code to change Region, ami, etc
+3. Security group which alows ports 22, 80 and 8080, we will change according to our requirements
--- Ec2-Jenkinsvm-Terraform/ec2-instance.tf
+++ Ec2-Jenkinsvm-Terraform/ec2-instance.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "Webserver" {
   ami           = data.aws_ami.amz2ami.id
   instance_type = var.instance_type
   key_name = var.keypair
-  user_data     = file("userdata.sh")
+  user_data     = file("userdata-jenkins.sh")
   vpc_security_group_ids = [aws_security_group.allow_http_ssh.id]
   tags = {
     "name" = "Amazon-Linux-Webserver"
