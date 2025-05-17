7115c884d2c0998b95cad7dd3689a83ab83a8ab5
Venu_DevOps <venugopalreddy1322@gmail.com>
2025-05-17 21:49:05 +0200

    more files updated and few files added

--- Ec2-Jenkinsvm-Terraform/ec2-securitygroups.tf
+++ Ec2-Jenkinsvm-Terraform/ec2-securitygroups.tf
@@ -32,11 +32,33 @@ resource "aws_vpc_security_group_ingress_rule" "allow_jenkins" {
   ip_protocol = "tcp"
   to_port = 8080
 }
+
 # Egress Rule
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
   security_group_id = aws_security_group.allow_http_ssh.id
+  security_group_id = aws_security_group.allow_custom_ports.id
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "-1" # semantically equivalent to all ports
 }
 
-
+resource "aws_security_group" "allow_cutom_ports" {
+  name        = "allow_custom"
+  description = "Allow custom TCP on port 5000 inbound traffic and all outbound traffic"
+  #vpc_id      = aws_vpc.main.id
+  tags = {
+    Name = "allow_custum_tcp"
+  }
+}
+resource "aws_vpc_security_group_ingress_rule" "allow_custom_traffic" {
+  security_group_id = aws_security_group.allow_cutom_ports.id
+  cidr_ipv4 = "0.0.0.0/0"
+  from_port = 5000
+  ip_protocol = "tcp"
+  to_port = 5000
+}
+# Egress Rule
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.allow_custom_ports.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
