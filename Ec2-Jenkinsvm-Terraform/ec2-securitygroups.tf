6478fd6da65dae5ada7ecd7a120c855555127fd8
Venu_DevOps <venugopalreddy1322@gmail.com>
2025-05-17 21:53:54 +0200

    more files updated and few files added

--- Ec2-Jenkinsvm-Terraform/ec2-securitygroups.tf
+++ Ec2-Jenkinsvm-Terraform/ec2-securitygroups.tf
@@ -36,17 +36,16 @@ resource "aws_vpc_security_group_ingress_rule" "allow_jenkins" {
 # Egress Rule
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
   security_group_id = aws_security_group.allow_http_ssh.id
-  security_group_id = aws_security_group.allow_custom_ports.id
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "-1" # semantically equivalent to all ports
 }
 
 resource "aws_security_group" "allow_cutom_ports" {
-  name        = "allow_custom"
+  name        = "allow_custom_ports"
   description = "Allow custom TCP on port 5000 inbound traffic and all outbound traffic"
   #vpc_id      = aws_vpc.main.id
   tags = {
-    Name = "allow_custum_tcp"
+    Name = "allow_custum_ports"
   }
 }
 resource "aws_vpc_security_group_ingress_rule" "allow_custom_traffic" {
